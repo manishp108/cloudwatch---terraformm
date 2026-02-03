b9b1b868ac71cb7258128eb2b7a7f71f280cd7ae
manishp108 <manish.developer18@gmail.com>
2026-02-03 14:14:16 +0530

    Exposed storage account blob endpoint Added outputs for Azure Key Vault details

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "resource_group_name" {
   value       = azurerm_resource_group.rg.name
   description = "Resource Group created"
 }
+
+output "storage_primary_blob_endpoint" {
+  value       = azurerm_storage_account.storage.primary_blob_endpoint
+  description = "Storage primary blob endpoint"
+}
+
+output "key_vault_name" {
+  value       = azurerm_key_vault.kv.name
+  description = "Key Vault name"
+}
+
+output "key_vault_uri" {
+  value       = azurerm_key_vault.kv.vault_uri
+  description = "Key Vault URI"
+}
